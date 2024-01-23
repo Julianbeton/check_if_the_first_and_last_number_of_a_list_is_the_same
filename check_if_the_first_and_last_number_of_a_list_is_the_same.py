32cec1a946e69a448808aa622a15eba38924dce7
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-23 23:03:05 +0800

    uploading final code

--- check_if_the_first_and_last_number_of_a_list_is_the_same.py
+++ check_if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -17,11 +17,13 @@ def checking_if_the_first_and_last_number_is_equal(given_list):
     first_number = given_list[0]
     last_number = given_list[4]
 
-    if first_number / last_number == 0:
+    if first_number / last_number == 1:
         return True
     else:
         return False
 
 numbers_x = [10, 20, 30, 40, 10]
+print("\033[1;32;40m", numbers_x,"The result in number_x given is", checking_if_the_first_and_last_number_is_equal(numbers_x))
 
 numbers_y = [75, 65, 35, 75, 30]
+print("\033[1;32;40m", numbers_y,"The result in number_x given is", checking_if_the_first_and_last_number_is_equal(numbers_y))
